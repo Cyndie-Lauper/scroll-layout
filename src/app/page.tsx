898dc4c6579a7820087322cb784054cad387ae1e
Minh <elevenine00@gmail.com>
2024-11-13 16:43:03 +0700

    initial Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image'
+import { Button } from '@/components'
 
 export default function Home() {
   return (
@@ -12,9 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
@@ -95,7 +97,8 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button label="Contact" />
       </footer>
     </div>
-  );
+  )
 }
