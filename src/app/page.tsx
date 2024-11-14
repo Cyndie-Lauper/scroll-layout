a437aa4585d34de5466d64c160f95cb0b54bd791
Minh <elevenine00@gmail.com>
2024-11-14 14:20:21 +0700

    feat: :sparkles: design and implement IntroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { HeaderSection } from '@/components'
+import { HeaderSection, IntroSection } from '@/components'
 
 /**
  * The main page of the website.
  * @returns {JSX.Element} JSX Element for the home page.
  */
 export default function Home(): JSX.Element {
-  return <HeaderSection />
+  return (
+    <>
+      <HeaderSection />
+      <IntroSection />
+    </>
+  )
 }
